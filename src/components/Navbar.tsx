fb05cac935bcc1286c70bf444c9a1c110e108ab6
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-07 21:00:11 -0800

    create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function NavBar({}: Props) {
+  return (
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>NavBar</nav>
+  )
+}
