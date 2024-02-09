09a5850e77a74f5b6a7ecf229ed1cc99bbfe41aa
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-08 21:48:02 -0800

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,84 @@
 
+'use client'
+
 import NavBar from "@/components/Navbar";
+import axios from "axios";
 import Image from "next/image";
+import { useQuery } from "react-query";
+
+//`https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
 
 export default function Home() {
+  // const [place, setPlace] = useAtom(placeAtom);
+  // const [loadingCity] = useAtom(loadingCityAtom);
+
+  const { isLoading, error, data, refetch } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    }
+  );
+
+  
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <NavBar />
