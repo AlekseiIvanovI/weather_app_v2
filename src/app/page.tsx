5c74543cd61f8d7cccc6a5cad674ed93c7249f98
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2024-02-09 21:17:54 -0800

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Container from "./components/Container";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
 
 
 
@@ -88,15 +89,39 @@ export default function Home() {
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt4">
-        <section>
-          <div>
-            <h2 className="flex gap-1 text-3xl items-end">
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
               <p className="text-lg">{format(parseISO(firstData?.dt_txt ?? ''), 'MM.dd.yyyy')}</p>
             </h2>
             <Container className="gap-10 px-6 items-center" >
               <div className="flex flex-col px-4">
-                
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>{convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}°↓</span>
+                  <span>{convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°↑</span>
+                </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div key={i} className="flex flex-col justify-between gap-2 items-center text-xs font-semibold">
+                      <p className="whitespace-nowrap">
+                        {format(parseISO(d.dt_txt), 'h:mm a')}
+
+                      </p>
+
+                      <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
+                  </div>
+                ))}
               </div>
             </Container>
           </div>
